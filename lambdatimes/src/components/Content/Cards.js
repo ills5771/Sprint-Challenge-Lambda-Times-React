7e41c123b9c218264d421c5e62ca82d2d0d1ec26
ills5771 <ilyayelizarov1@gmail.com>
2019-02-22 09:01:02 -0800

    added map function in cards.js to return single instance of Card

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
-import Card from './Card';
+import React, { Component } from "react";
+import Card from "./Card";
 
 const Cards = props => {
   return (
     <div className="cards-container">
+      {props.cards.map(card => (
+        <Card
+          tab={card.tab}
+          headline={card.headline}
+          img={card.img}
+          author={card.author}
+        />
+      ))}
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include prop types for all of your incoming props
 
